c79f1b9dd0dc9d489ba55c50f428c1b52f5d3b7f
lil_nurik <98051184+Buriev-Nurmuhammad@users.noreply.github.com>
2022-02-24 21:26:59 +0500

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,81 @@
+//let a=2, b=3, c=4;
+
+
+//console.log("our variabes",a,b,c);
+
+/*let name="Alex";
+
+name="huy"
+
+console.log(name);*/
+
+/*let a=4;
+let b=a;*/
+
+//const PI=3.14;
+
+/*const COLOR_BLUE = "#23AB4C" 
+const COLOR_RED = "#32FB42" 
+
+let color= COLOR_RED;
+
+console.log(color)
+console.log(color)
+console.log(color)
+console.log(color)
+console.log(color)
+console.log(color)
+*/
+ 
+/*let huy = "huy"
+
+console.log(huy)
+
+//let number1 = 01110000011110010111010001101000011011110110111000100000011101000110111101110000;
+//console.log(number1);
+
+/*const a = "01110000011110010111010001101000011011110110111000100000011101000110111101110000";
+console.log(a);*/
+
+
+
+
+
+
+
+
+
+
+/*let calc = +prompt("Первая цифра")
+let calc2 = +prompt("Вторая цифра")
+
+console.log(`сумма чисел: ${calc+calc2}`);
+console.log(`произведение чисел: ${calc*calc2}`);
+console.log(`разность чисел: ${calc-calc2}`);
+console.log(`частное чисел: ${calc/calc2}`);*/
+//console.log(calc + 2)
+
+
+
+let calc = prompt("Введите арифметическое выражение")
+
+alert(eval(calc) )
+console.log(eval(calc));
+
+
+
+
+
+/*let name = "Нурик";
+let age = '16';
+let step = '1';
+
+let sentence = `Меня зовут ${name}, мне ${age} лет и я учусь в ${step} курсе`;
+
+console.log(sentence)*/
+
+
+
+
+
+                                
